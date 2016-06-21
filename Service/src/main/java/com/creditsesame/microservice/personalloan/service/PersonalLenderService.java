56e5595e9a71f100af5f697196aad7a0291059e4
Francisco Martin <franjmartin21@gmail.com>
2016-06-21 12:54:36 -0700

    Added support for transactionality

--- Service/src/main/java/com/creditsesame/microservice/personalloan/service/PersonalLenderService.java
+++ Service/src/main/java/com/creditsesame/microservice/personalloan/service/PersonalLenderService.java
@@ -4,13 +4,15 @@ import com.creditsesame.microservice.personalloan.dao.PersonalLenderDao;
 import com.creditsesame.microservice.personalloan.domain.PersonalLender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
     Normal Service class
 
     TO BE DONE:
-    - Transaction support with Spring transaction??
+    - Transaction support with Spring transaction?? This needs to be configured in the propagation and isolation required
  */
+@Transactional
 @Service
 public class PersonalLenderService {
 
